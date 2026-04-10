ff6fc7f99083566937f89c20b8066afa04cc0695
Adriano <adriytkr@gmail.com>
2026-04-09 23:50:59 -0300

    learn Lark

--- src/main.py
+++ src/main.py
@@ -1,6 +1,15 @@
 from lark import Lark,Transformer
 
 class MyData(Transformer):
+  def ID(self,token):
+    return str(token)
+
+  def arg_list(self,children):
+    children
+
+  def statement(self,children):
+    return children[0]
+
   def calculation(self,children):
     left=int(children[0])
     op=children[1]
@@ -15,13 +24,19 @@ class MyData(Transformer):
 
     return ops[op](left, right)
 
+  def func(self, children):
+    func_name=children[0]
+    args=children[1]
+    body=children[2:] 
+    return f"Defined function {func_name} with args {args}"
+
   def start(self,children):
     return children
 
 with open('./grammar.lark','r') as f:
   parser=Lark(f.read())
 
-  text='5+3'
+  text='5+3; 3+10; func f(x){3*5;}'
   tree=parser.parse(text)
   print(tree.pretty())
 
